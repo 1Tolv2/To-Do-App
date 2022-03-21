ae7d486b214b79728061753cb2793535ae88b0f5
1Tolv2 <sofiaq1@gmail.com>
2022-03-21 16:36:24 +0100

    Added routes

--- api/app.js
+++ api/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { PORT } = require("./settings");
+const mongoose = require("mongoose");
 
-const indexRouter = require("./routes/index");
+const { PORT } = require("./settings");
+const userRouter = require("./routes/users");
+const postRouter = require("./routes/posts");
 
 const app = express();
 
 app.use("/", indexRouter);
+
+mongoose.connect("mongodb://localhost/toDo");
 
 app.listen(PORT, (req, res) => {
   console.log(`Started express server on PORT: ${PORT}`);
