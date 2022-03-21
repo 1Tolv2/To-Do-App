2eefd6fa9db1f9857951ffe0ad168960ff5bf56f
1Tolv2 <sofiaq1@gmail.com>
2022-03-21 23:24:31 +0100

    Cleaned up unnecessary code in controllers/auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,17 +4,16 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const createUser = async (req, res) => {
   const { username, password } = req.body;
-  const modifiedUsername = username.toLowerCase();
 
   if (!(username && password)) {
     res.json({ error: "Invalid data. Username and password is required." });
   } else {
-    if (await User.findOne({ username: modifiedUsername })) {
+    if (await User.findOne({ username: username.toLowerCase() })) {
       res
         .status(400)
         .json({ error: "User already exists, username must be unique" });
     } else {
-      const user = new User({ username: modifiedUsername, password });
+      const user = new User({ username: username.toLowerCase(), password });
       await user.save();
       res.json({ user: user.username });
     }
@@ -23,14 +22,9 @@ const createUser = async (req, res) => {
 
 const logInUser = async (req, res) => {
   const { username, password } = req.body;
-  const modifiedUsername = username.toLowerCase();
-  console.log(username);
-  console.log(password);
 
-  const user = await User.login(modifiedUsername, password);
+  const user = await User.login(username.toLowerCase(), password);
   if (user) {
-    console.log(JWT_SECRET);
-
     const userId = user._id.toString();
     const token = jwt.sign({ userId, username: user.username }, JWT_SECRET, {
       expiresIn: 120,
