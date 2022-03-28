dd5d3037181a331292f5b33ffc87ba68e5e18922
1Tolv2 <sofiaq1@gmail.com>
2022-03-28 23:25:42 +0200

    changed name

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CenteredDivLayout from "../components/layouts/centeredDivLayout";
-import FormBox from "../components/organisms/FormBox";
+import LoginTabs from "../components/organisms/LoginTabs";
 import "../styles/Home.module.css";
 
 export default function Login() {
   return (
     <CenteredDivLayout>
-      <FormBox></FormBox>
+      <LoginTabs></LoginTabs>
     </CenteredDivLayout>
   );
 }
