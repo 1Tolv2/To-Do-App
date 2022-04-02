a15928225cbec195172d3c955ade964afc75ca03
1Tolv2 <sofiaq1@gmail.com>
2022-04-02 17:24:50 +0200

    Added mainLayout

---
+++ client/components/layouts/mainLayout/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as s from './styles'
+
+
+
+export const MainLayout = () => {
+  return (
+    <><s.GlobalCSS/>MainLayout</>
+  )
+}
---
+++ client/components/layouts/mainLayout/styles.jsx
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+
+const GlobalCSS = createGlobalStyle`
+body {
+  background-color: #AD7A99;
+  color: black;
+}
+`;
+
+export {GlobalCSS}
