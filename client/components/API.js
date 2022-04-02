7a06fd8fad5d7c7dd382c4e11369aa619b415f1e
1Tolv2 <sofiaq1@gmail.com>
2022-04-02 17:22:20 +0200

    Added API.js

---
+++ client/components/API.js
@@ -0,0 +1,23 @@
+const PORT = 9000;
+const APIURL = `http://localhost:${PORT}`;
+
+const registerUser = async (payload) => {
+  const res = await fetch(`${APIURL}/auth/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+  return res.ok;
+};
+
+const logInUser = async (payload) => {
+  const res = await fetch(`${APIURL}/auth/api-token`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+  const data = await res.json();
+  return data;
+};
+
+export { registerUser, logInUser };
