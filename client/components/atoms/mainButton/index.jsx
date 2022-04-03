bc01ae4c0b1d2ea86fa8d2e24a366914ada7c6ae
1Tolv2 <sofiaq1@gmail.com>
2022-04-03 21:41:48 +0200

    Added button type and refactored styles

--- client/components/atoms/mainButton/index.jsx
+++ client/components/atoms/mainButton/index.jsx
@@ -8,6 +8,10 @@ export const MainButton = ({ type, children, fullWidth, bgColor }) => {
         <s.RoundButton bgColor={bgColor} fullWidth={fullWidth}>
           {children}
         </s.RoundButton>
+      ) : type === "inverted" ? (
+        <s.InvertedButton bgColor={bgColor} fullWidth={fullWidth}>
+          {children}
+        </s.InvertedButton>
       ) : (
         <s.Button bgColor={bgColor} fullWidth={fullWidth}>
           {children}
--- client/components/atoms/mainButton/styles.jsx
+++ client/components/atoms/mainButton/styles.jsx
@@ -1,29 +1,44 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../../styles";
 
-const Button = styled.button`
-  width: ${props => props.fullWidth ? "100%" : "fit-content"};
+const BaseButton = css`
+  width: ${(props) => (props.fullWidth ? "100%" : "fit-content")};
   margin: 30px 0;
   padding: 10px;
-  border: none;
-  border-radius: 25px;
   font-size: 1.1em;
   font-weight: 600;
+  border-radius: 25px;
   text-transform: uppercase;
-  background-color: ${({ bgColor }) => (bgColor ? bgColor : theme.colors.greenMain)};
-  color: white;
   cursor: pointer;
 `;
 
+const Button = styled.button`
+  ${BaseButton}
+  border: none;
+  background-color: ${({ bgColor }) =>
+    bgColor ? theme.colors[bgColor] : theme.colors.greenMain};
+  color: white;
+`;
+
+const InvertedButton = styled.button`
+  ${BaseButton}
+  background-color: transparent;
+  color: ${({ bgColor }) =>
+    bgColor ? theme.colors[bgColor] : theme.colors.greenMain};
+  border: 2px solid
+    ${({ bgColor }) =>
+      bgColor ? theme.colors[bgColor] : theme.colors.greenMain};
+`;
+
 const RoundButton = styled.button`
-width: 40px;
-height: 40px;
-margin: auto;
-border-radius: 25px;
-font-size: 1.8em;
-padding-bottom: 3px;
-border: none;
-background-color: white;
-cursor: pointer;
-`
-export { Button, RoundButton };
+  width: 40px;
+  height: 40px;
+  margin: auto;
+  border-radius: 25px;
+  font-size: 1.8em;
+  padding-bottom: 3px;
+  border: none;
+  background-color: white;
+  cursor: pointer;
+`;
+export { Button, RoundButton, InvertedButton };
