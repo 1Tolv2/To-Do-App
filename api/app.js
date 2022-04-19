d2e4fc10d897595aa6bfa204d701df034ff0e99d
1Tolv2 <sofiaq1@gmail.com>
2022-04-19 13:32:59 +0200

    BE: added token error handling

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const multer = require("multer");
 require("dotenv").config();
 
+const { findExpiredToken } = require("./models/expiredToken");
 const { PORT } = require("./settings");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
@@ -25,13 +26,28 @@ app.use("/uploads", express.static("./uploads"));
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   const authHeader = req.header("Authorization");
 
+  // kolla att authHeader är Bearer
   if (authHeader && authHeader.split(" ")[0] === "Bearer") {
-    const token = authHeader.split(" ")[1];
-    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    const token = authHeader.split(" ")[1]; // splitta så vi får ut tokenen
+    const tokenLoggedOut = await findExpiredToken();
+    if (tokenLoggedOut) {
+      res.status(401).json({ error: "Invalid token" });
+    } else {
+      try {
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
+      } catch (error) {
+        return error.message === "jwt expired"
+          ? res.status(401).json({ error: "Token expired" })
+          : error.message === "invalid token"
+          ? res.status(401).json({ error: "Invalid token" })
+          : res.status(400).json({ error: "Token error" });
+      }
+    }
   }
+
   next();
 });
 
