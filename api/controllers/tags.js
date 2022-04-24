4ee86ea730bf97e8e9fd1a7c65a508ff6c1010dd
1Tolv2 <sofiaq1@gmail.com>
2022-04-24 14:25:22 +0200

    make sure user is not added twice tot ag

--- api/controllers/tags.js
+++ api/controllers/tags.js
@@ -13,11 +13,16 @@ const getAllTags = async (req, res) => {
 const handleNewTag = async (req, res) => {
   const body = req.body;
   const check = await getTagByTagName(body.tagName);
-  check.length > 0
-    ? await addUserToTag(body.tagName, req.user.userId)
-    : await createNewTag(body.tagName, req.user.userId);
-
-  res.json({ message: "successfully added tag" });
+  console.log(check[0]);
+  if (check[0].user.includes(req.user.userId)) {
+    res.status(400).json({ error: "User already has tag" });
+  } else if (check.length > 0) {
+    await addUserToTag(body.tagName, req.user.userId);
+    res.json({ message: "successfully added user to tag" });
+  } else if ((check.length = 0)) {
+    await createNewTag(body.tagName, req.user.userId);
+    res.json({ message: "successfully added user to tag" });
+  }
 };
 
 module.exports = { getAllTags, handleNewTag };
